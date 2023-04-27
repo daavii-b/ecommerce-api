f8f4cf9bcf4dfae3643840cd87a10cd06479d7a3
daavii-b <britodavi122@gmail.com>
2023-04-27 20:22:20 -0300

    fix: fixing conf in settings

--- core/settings.py
+++ core/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -182,7 +183,7 @@ SIMPLE_JWT = {
 # Media
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # SMTP
 
